ccfb3a5df11346685deca913fdac9d3deabd6b38
Bernard Mordan <bernardmordan@gmail.com>
2014-08-03 12:30:10 +0100

    sunday afternoon

--- z_layouts/PopOutMenu/main.js
+++ z_layouts/PopOutMenu/main.js
@@ -1,8 +1,12 @@
 define(function(require, exports, module) {
     // import dependencies
     var Engine = require('famous/core/Engine')
-    var AppView = require('./AppView')
+    var Surface = require('famous/core/Surface')
+    var Modifier = require('famous/core/Modifier')
+    var Transform = require('famous/core/Transform')
+    var ImageSurface = require('famous/surfaces/ImageSurface')
+
+    // create the main context
     var mainContext = Engine.createContext()
-    var appView = new AppView();
-    mainContext.add(appView)
+
 });
